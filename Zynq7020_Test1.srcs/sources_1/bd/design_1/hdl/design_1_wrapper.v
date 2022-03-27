0acfbfa8decd8cf6a7f0caaa4abb5da8c05f7672
LX050724 <yaojizhong@outlook.com>
2022-03-27 13:54:58 +0800

    补充BD文件，并优化忽略文件

---
+++ Zynq7020_Test1.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -0,0 +1,249 @@
+//Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+//Date        : Sun Mar 27 12:53:10 2022
+//Host        : DESKTOP-PLVBPLM running 64-bit major release  (build 9200)
+//Command     : generate_target design_1_wrapper.bd
+//Design      : design_1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_1_wrapper
+   (ADC_Port_0_adc_clk,
+    ADC_Port_0_adc_data,
+    DAC_Port_0_clk,
+    DAC_Port_0_dac_data,
+    DDR_addr,
+    DDR_ba,
+    DDR_cas_n,
+    DDR_ck_n,
+    DDR_ck_p,
+    DDR_cke,
+    DDR_cs_n,
+    DDR_dm,
+    DDR_dq,
+    DDR_dqs_n,
+    DDR_dqs_p,
+    DDR_odt,
+    DDR_ras_n,
+    DDR_reset_n,
+    DDR_we_n,
+    FIXED_IO_ddr_vrn,
+    FIXED_IO_ddr_vrp,
+    FIXED_IO_mio,
+    FIXED_IO_ps_clk,
+    FIXED_IO_ps_porb,
+    FIXED_IO_ps_srstb,
+    GPIO_0_0_tri_io,
+    HDMI_CLK_N_0,
+    HDMI_CLK_P_0,
+    HDMI_D0_N_0,
+    HDMI_D0_P_0,
+    HDMI_D1_N_0,
+    HDMI_D1_P_0,
+    HDMI_D2_N_0,
+    HDMI_D2_P_0,
+    IIC_0_0_scl_io,
+    IIC_0_0_sda_io,
+    IIC_1_0_scl_io,
+    IIC_1_0_sda_io,
+    In2_0);
+  output ADC_Port_0_adc_clk;
+  input [7:0]ADC_Port_0_adc_data;
+  output DAC_Port_0_clk;
+  output [7:0]DAC_Port_0_dac_data;
+  inout [14:0]DDR_addr;
+  inout [2:0]DDR_ba;
+  inout DDR_cas_n;
+  inout DDR_ck_n;
+  inout DDR_ck_p;
+  inout DDR_cke;
+  inout DDR_cs_n;
+  inout [3:0]DDR_dm;
+  inout [31:0]DDR_dq;
+  inout [3:0]DDR_dqs_n;
+  inout [3:0]DDR_dqs_p;
+  inout DDR_odt;
+  inout DDR_ras_n;
+  inout DDR_reset_n;
+  inout DDR_we_n;
+  inout FIXED_IO_ddr_vrn;
+  inout FIXED_IO_ddr_vrp;
+  inout [53:0]FIXED_IO_mio;
+  inout FIXED_IO_ps_clk;
+  inout FIXED_IO_ps_porb;
+  inout FIXED_IO_ps_srstb;
+  inout [3:0]GPIO_0_0_tri_io;
+  output HDMI_CLK_N_0;
+  output HDMI_CLK_P_0;
+  output HDMI_D0_N_0;
+  output HDMI_D0_P_0;
+  output HDMI_D1_N_0;
+  output HDMI_D1_P_0;
+  output HDMI_D2_N_0;
+  output HDMI_D2_P_0;
+  inout IIC_0_0_scl_io;
+  inout IIC_0_0_sda_io;
+  inout IIC_1_0_scl_io;
+  inout IIC_1_0_sda_io;
+  input [0:0]In2_0;
+
+  wire ADC_Port_0_adc_clk;
+  wire [7:0]ADC_Port_0_adc_data;
+  wire DAC_Port_0_clk;
+  wire [7:0]DAC_Port_0_dac_data;
+  wire [14:0]DDR_addr;
+  wire [2:0]DDR_ba;
+  wire DDR_cas_n;
+  wire DDR_ck_n;
+  wire DDR_ck_p;
+  wire DDR_cke;
+  wire DDR_cs_n;
+  wire [3:0]DDR_dm;
+  wire [31:0]DDR_dq;
+  wire [3:0]DDR_dqs_n;
+  wire [3:0]DDR_dqs_p;
+  wire DDR_odt;
+  wire DDR_ras_n;
+  wire DDR_reset_n;
+  wire DDR_we_n;
+  wire FIXED_IO_ddr_vrn;
+  wire FIXED_IO_ddr_vrp;
+  wire [53:0]FIXED_IO_mio;
+  wire FIXED_IO_ps_clk;
+  wire FIXED_IO_ps_porb;
+  wire FIXED_IO_ps_srstb;
+  wire [0:0]GPIO_0_0_tri_i_0;
+  wire [1:1]GPIO_0_0_tri_i_1;
+  wire [2:2]GPIO_0_0_tri_i_2;
+  wire [3:3]GPIO_0_0_tri_i_3;
+  wire [0:0]GPIO_0_0_tri_io_0;
+  wire [1:1]GPIO_0_0_tri_io_1;
+  wire [2:2]GPIO_0_0_tri_io_2;
+  wire [3:3]GPIO_0_0_tri_io_3;
+  wire [0:0]GPIO_0_0_tri_o_0;
+  wire [1:1]GPIO_0_0_tri_o_1;
+  wire [2:2]GPIO_0_0_tri_o_2;
+  wire [3:3]GPIO_0_0_tri_o_3;
+  wire [0:0]GPIO_0_0_tri_t_0;
+  wire [1:1]GPIO_0_0_tri_t_1;
+  wire [2:2]GPIO_0_0_tri_t_2;
+  wire [3:3]GPIO_0_0_tri_t_3;
+  wire HDMI_CLK_N_0;
+  wire HDMI_CLK_P_0;
+  wire HDMI_D0_N_0;
+  wire HDMI_D0_P_0;
+  wire HDMI_D1_N_0;
+  wire HDMI_D1_P_0;
+  wire HDMI_D2_N_0;
+  wire HDMI_D2_P_0;
+  wire IIC_0_0_scl_i;
+  wire IIC_0_0_scl_io;
+  wire IIC_0_0_scl_o;
+  wire IIC_0_0_scl_t;
+  wire IIC_0_0_sda_i;
+  wire IIC_0_0_sda_io;
+  wire IIC_0_0_sda_o;
+  wire IIC_0_0_sda_t;
+  wire IIC_1_0_scl_i;
+  wire IIC_1_0_scl_io;
+  wire IIC_1_0_scl_o;
+  wire IIC_1_0_scl_t;
+  wire IIC_1_0_sda_i;
+  wire IIC_1_0_sda_io;
+  wire IIC_1_0_sda_o;
+  wire IIC_1_0_sda_t;
+  wire [0:0]In2_0;
+
+  IOBUF GPIO_0_0_tri_iobuf_0
+       (.I(GPIO_0_0_tri_o_0),
+        .IO(GPIO_0_0_tri_io[0]),
+        .O(GPIO_0_0_tri_i_0),
+        .T(GPIO_0_0_tri_t_0));
+  IOBUF GPIO_0_0_tri_iobuf_1
+       (.I(GPIO_0_0_tri_o_1),
+        .IO(GPIO_0_0_tri_io[1]),
+        .O(GPIO_0_0_tri_i_1),
+        .T(GPIO_0_0_tri_t_1));
+  IOBUF GPIO_0_0_tri_iobuf_2
+       (.I(GPIO_0_0_tri_o_2),
+        .IO(GPIO_0_0_tri_io[2]),
+        .O(GPIO_0_0_tri_i_2),
+        .T(GPIO_0_0_tri_t_2));
+  IOBUF GPIO_0_0_tri_iobuf_3
+       (.I(GPIO_0_0_tri_o_3),
+        .IO(GPIO_0_0_tri_io[3]),
+        .O(GPIO_0_0_tri_i_3),
+        .T(GPIO_0_0_tri_t_3));
+  IOBUF IIC_0_0_scl_iobuf
+       (.I(IIC_0_0_scl_o),
+        .IO(IIC_0_0_scl_io),
+        .O(IIC_0_0_scl_i),
+        .T(IIC_0_0_scl_t));
+  IOBUF IIC_0_0_sda_iobuf
+       (.I(IIC_0_0_sda_o),
+        .IO(IIC_0_0_sda_io),
+        .O(IIC_0_0_sda_i),
+        .T(IIC_0_0_sda_t));
+  IOBUF IIC_1_0_scl_iobuf
+       (.I(IIC_1_0_scl_o),
+        .IO(IIC_1_0_scl_io),
+        .O(IIC_1_0_scl_i),
+        .T(IIC_1_0_scl_t));
+  IOBUF IIC_1_0_sda_iobuf
+       (.I(IIC_1_0_sda_o),
+        .IO(IIC_1_0_sda_io),
+        .O(IIC_1_0_sda_i),
+        .T(IIC_1_0_sda_t));
+  design_1 design_1_i
+       (.ADC_Port_0_adc_clk(ADC_Port_0_adc_clk),
+        .ADC_Port_0_adc_data(ADC_Port_0_adc_data),
+        .DAC_Port_0_clk(DAC_Port_0_clk),
+        .DAC_Port_0_dac_data(DAC_Port_0_dac_data),
+        .DDR_addr(DDR_addr),
+        .DDR_ba(DDR_ba),
+        .DDR_cas_n(DDR_cas_n),
+        .DDR_ck_n(DDR_ck_n),
+        .DDR_ck_p(DDR_ck_p),
+        .DDR_cke(DDR_cke),
+        .DDR_cs_n(DDR_cs_n),
+        .DDR_dm(DDR_dm),
+        .DDR_dq(DDR_dq),
+        .DDR_dqs_n(DDR_dqs_n),
+        .DDR_dqs_p(DDR_dqs_p),
+        .DDR_odt(DDR_odt),
+        .DDR_ras_n(DDR_ras_n),
+        .DDR_reset_n(DDR_reset_n),
+        .DDR_we_n(DDR_we_n),
+        .FIXED_IO_ddr_vrn(FIXED_IO_ddr_vrn),
+        .FIXED_IO_ddr_vrp(FIXED_IO_ddr_vrp),
+        .FIXED_IO_mio(FIXED_IO_mio),
+        .FIXED_IO_ps_clk(FIXED_IO_ps_clk),
+        .FIXED_IO_ps_porb(FIXED_IO_ps_porb),
+        .FIXED_IO_ps_srstb(FIXED_IO_ps_srstb),
+        .GPIO_0_0_tri_i({GPIO_0_0_tri_i_3,GPIO_0_0_tri_i_2,GPIO_0_0_tri_i_1,GPIO_0_0_tri_i_0}),
+        .GPIO_0_0_tri_o({GPIO_0_0_tri_o_3,GPIO_0_0_tri_o_2,GPIO_0_0_tri_o_1,GPIO_0_0_tri_o_0}),
+        .GPIO_0_0_tri_t({GPIO_0_0_tri_t_3,GPIO_0_0_tri_t_2,GPIO_0_0_tri_t_1,GPIO_0_0_tri_t_0}),
+        .HDMI_CLK_N_0(HDMI_CLK_N_0),
+        .HDMI_CLK_P_0(HDMI_CLK_P_0),
+        .HDMI_D0_N_0(HDMI_D0_N_0),
+        .HDMI_D0_P_0(HDMI_D0_P_0),
+        .HDMI_D1_N_0(HDMI_D1_N_0),
+        .HDMI_D1_P_0(HDMI_D1_P_0),
+        .HDMI_D2_N_0(HDMI_D2_N_0),
+        .HDMI_D2_P_0(HDMI_D2_P_0),
+        .IIC_0_0_scl_i(IIC_0_0_scl_i),
+        .IIC_0_0_scl_o(IIC_0_0_scl_o),
+        .IIC_0_0_scl_t(IIC_0_0_scl_t),
+        .IIC_0_0_sda_i(IIC_0_0_sda_i),
+        .IIC_0_0_sda_o(IIC_0_0_sda_o),
+        .IIC_0_0_sda_t(IIC_0_0_sda_t),
+        .IIC_1_0_scl_i(IIC_1_0_scl_i),
+        .IIC_1_0_scl_o(IIC_1_0_scl_o),
+        .IIC_1_0_scl_t(IIC_1_0_scl_t),
+        .IIC_1_0_sda_i(IIC_1_0_sda_i),
+        .IIC_1_0_sda_o(IIC_1_0_sda_o),
+        .IIC_1_0_sda_t(IIC_1_0_sda_t),
+        .In2_0(In2_0));
+endmodule
---
+++ Zynq7020_Test1.srcs/sources_1/bd/design_1/hw_handoff/design_1_bd.tcl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:304b5061211630778255fbae2bfe910a8642b037f078beaef46773370ef6afea
+size 75627
